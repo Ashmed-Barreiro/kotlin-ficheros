99b7a7cd974a0d589279c08c57082976700d80e8
Ashmed Barreiro <ashmed.barreiro.7e8@itb.cat>
2025-06-05 19:13:16 +0200

    Update Ficheros.kt añadido mas contenido de ficheros

--- kotlin-ficheros-recu/src/Ficheros.kt
+++ kotlin-ficheros-recu/src/Ficheros.kt
@@ -23,3 +23,57 @@ fun main() {
     }
     println("Resumen generado en txt/resumen.txt")
 }
+
+//Más trabajo con ficheros, entrada y lectura de datos (nombres y notas de alumnos)
+
+fun main() {
+
+    //bloque guarda datos en archivo
+ fun guardarEnArchivo(){
+    val resumen = File("txt/resumen.txt")
+    var notasAlumnos = mutableMapOf<String,Int>()
+    println("Hola, bienvenido al gestor de notas mas guay del mundo...")
+    println("Dime las notas de 5 alumnos separado por ':' (Ej: Ana:5)")
+    while (notasAlumnos.size<5){
+        var alumno = readln()
+        var nombre = alumno.split(":")[0]
+        var nota = alumno.split(":")[1].toInt()
+        notasAlumnos.put(nombre,nota)
+    }
+    //El orden de las variables importa fijate!!!
+    for ((alumno,nota)in notasAlumnos){
+        resumen.appendText("$alumno:$nota\n")
+    }
+ }
+    guardarEnArchivo()
+
+    //bloque leer archivo sacar media, y mayor nota
+    fun leerDeArchivo(){
+        val datos =File("txt/resumen.txt")
+        var datosAlumno= mutableMapOf<String,Int>()
+        var notaMayor = 0
+        var nombreNota = ""
+        var media = 0
+
+        datos.forEachLine { line ->
+            val partes = line.split(":")
+            val nombre = partes[0]
+            val nota = partes[1].toInt()
+            datosAlumno[nombre] = nota
+        }
+
+        for ((alumno,nota) in datosAlumno){
+            media += nota
+            if (nota>notaMayor){
+                notaMayor = nota
+                nombreNota = alumno
+            }
+        }
+        media = media/datosAlumno.size
+        println("El alumno con mayor nota es: $nombreNota ;)")
+        println("")
+        println("la nota media es de: $media")
+    }
+
+    leerDeArchivo()
+}
